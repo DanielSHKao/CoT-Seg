d8ca6146c41e6f742ecd118652c2dbb6b91c9b99
HUANG, Chak Ho <44686316+HarlezCode@users.noreply.github.com>
2025-11-10 06:25:27 +0100

    Update Dataset

---
+++ dataset/read_dataset.py
@@ -0,0 +1,32 @@
+import os
+import json
+
+class CustomDataset:
+    def __init__(self, path):
+        self.json_files = [path+f for f in os.listdir(path) if f.endswith('.json')]
+        self.arr = []
+        for f in self.json_files:
+            with open(f, 'r', encoding='utf-8') as file:
+                d = json.load(file)
+                if 'text' in d.keys():
+                    img_path = f.split('.json')[0].split("/")[-1]
+                    for i in d['text']:
+                        self.arr.append((img_path+".jpg", i, img_path+"_mask.png"))
+                else:
+                    l = len(d.keys()) - 1
+                    for i in range(l):
+                        self.arr.append((d['img'], d[str(i)], d['img'].split(".")[0]+"_mask.png"))
+                file.close()
+    def __getitem__(self, index):
+        return self.arr[index]
+    def __len__(self):
+        return len(self.arr)
+
+
+if __name__ == "__main__":
+    ds = CustomDataset("../../cot-seg_code/datasets/new_dataset/final/")
+    for i in range(len(ds)):
+        print(ds[i])
+
+
+
---
+++ dataset/readme.md
@@ -0,0 +1,6 @@
+## ReasonSeg-Hard
+We have 79 Images in this dataset most are sampled from ReasonSeg-Test, the dataset can be downloaded from
+[this link](https://drive.google.com/file/d/1rPLPF_Ftk-YHXLvBpowy_aSpBMlu2fDp/view?usp=drive_link).
+
+If you read the dataset using the class provided in read_dataset.py, entries will be in the format of
+(img_name, query, mask_name).
